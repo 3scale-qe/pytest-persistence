01c0470d11f5e7b61640e7817927910a76aa83e3
Marian Ganisin <23635632+mganisin@users.noreply.github.com>
2024-08-18 10:07:53 +0200

    Tests extended to cover multiple pytest versions

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -10,23 +10,37 @@ def test_addoption(pytestconfig):
 
 
 def test_store():
-    os.system("pytest --store stored_tests test_mock.py")
+    os.system("pytest --store stored_tests tests/test_mock.py")
     with open("stored_tests", 'rb') as f:
         data = pickle.load(f)
-        assert data == {'session': {}, 'package': {}, 'module': {}, 'class': {}, 'function': {
-            'tests/test_mock.py::test1': {"('fixture1', 'function', 'tests/test_mock.py', None)": 42}},
-                        'workers': {'tests/test_mock.py::test1': None}}
+        assert data == {
+            'class': {},
+            'function': {'tests/test_mock.py::test1': {"('fixture1', 'function', 'tests/test_mock.py', None)": 42},
+                         'tests/test_mock.py::test2': {"('fixture2', 'function', 'tests/test_mock.py', None)": 'tmp'}},
+            'module': {},
+            'package': {},
+            'session': {"('cleanup', 'session', 'tests/test_mock.py', None)": True},
+            'tests': {'tests/test_mock.py::test1': None,
+                      'tests/test_mock.py::test2': None},
+            'workers': {'tests/test_mock.py::test1': None,
+                         'tests/test_mock.py::test2': None}}
+
+        assert os.path.isdir("tmp")
 
 
 def test_store_and_load(request):
     request.addfinalizer(lambda: os.remove('stored_tests'))
-    os.system("pytest --store stored_tests test_mock.py")
+    os.system("pytest --store stored_tests tests/test_mock.py")
     stream = os.popen('ls').read()
     assert "stored_tests" in stream.split('\n')
 
-    stream = os.popen("pytest --load stored_tests test_mock.py").read()
+    assert os.path.isdir("tmp")
+
+    stream = os.popen("pytest --load stored_tests tests/test_mock.py").read()
     assert "test_mock.py ." in stream
-    assert "1 passed" in stream
+    assert "2 passed" in stream
+
+    assert os.path.isdir("tmp")
 
 
 def test_store_error(request):
@@ -34,12 +48,12 @@ def test_store_error(request):
     stream = str(subprocess.Popen("pytest --store", shell=True, stderr=subprocess.PIPE).stderr.read())
     assert "pytest: error: argument --store: expected one argument" in stream
 
-    stream = str(subprocess.Popen("pytest --store test_mock.py", shell=True, stdout=subprocess.PIPE).stdout.read())
+    stream = str(subprocess.Popen("pytest --store tests/test_mock.py", shell=True, stdout=subprocess.PIPE).stdout.read())
     assert "FileExistsError: This file already exists" in stream
 
     os.mknod("stored_tests")
     stream = str(
-        subprocess.Popen("pytest --store stored_tests test_mock.py", shell=True, stdout=subprocess.PIPE).stdout.read())
+        subprocess.Popen("pytest --store stored_tests tests/test_mock.py", shell=True, stdout=subprocess.PIPE).stdout.read())
     assert "FileExistsError: This file already exists" in stream
 
 
